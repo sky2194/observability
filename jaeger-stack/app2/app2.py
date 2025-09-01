194e8d09bc5e2431ec93eda464415c9afd9e52fe
saik <koutilya183@gmail.com>
2025-09-01 19:05:22 -0400

    Tracing

---
+++ jaeger-stack/app2/app2.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from opentelemetry import trace
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+app = Flask(__name__)
+
+# Configure Tracer
+trace.set_tracer_provider(TracerProvider())
+jaeger_exporter = JaegerExporter(
+    agent_host_name="jaeger",  # matches service name in docker-compose
+    agent_port=6831,
+)
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(jaeger_exporter)
+)
+tracer = trace.get_tracer(__name__)
+
+@app.route("/")
+def hello():
+    with tracer.start_as_current_span("hello-span"):
+        return "Hello from App1 with Jaeger tracing!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8002)
